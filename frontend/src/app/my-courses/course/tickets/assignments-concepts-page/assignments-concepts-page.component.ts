184bdefc7b7515b996d0171ec0ad8c3f837dfe87
Cassidy Lowe <calowe@unc.edu>
2025-04-05 19:33:40 -0400

    added some css and html to the files to set up going to the backend

--- frontend/src/app/my-courses/course/tickets/assignments-concepts-page/assignments-concepts-page.component.ts
+++ frontend/src/app/my-courses/course/tickets/assignments-concepts-page/assignments-concepts-page.component.ts
@@ -1,12 +1,1 @@
-import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-assignments-concepts-page',
-  standalone: true,
-  imports: [],
-  templateUrl: './assignments-concepts-page.component.html',
-  styleUrl: './assignments-concepts-page.component.css'
-})
-export class AssignmentsConceptsPageComponent {
-
-}
