b0f967968027bbfd7e96ce4c7e1712fea5f5d1dc
Cassidy Lowe <calowe@unc.edu>
2025-04-06 10:56:20 -0400

    added trishas changes so no issues merging bc it was being weird

--- frontend/src/app/my-courses/course/tickets/assignments-concepts-page/assignments-concepts-page.component.ts
+++ frontend/src/app/my-courses/course/tickets/assignments-concepts-page/assignments-concepts-page.component.ts
@@ -1,1 +1,20 @@
+import { Component } from '@angular/core';
+import { courseSitePageGuard } from '../../office-hours/office-hours.guard';
 
+@Component({
+  selector: 'app-assignments-concepts-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './assignments-concepts-page.component.html',
+  styleUrl: './assignments-concepts-page.component.css'
+})
+export class AssignmentsConceptsPageComponent {
+  /** Route information to be used in the routing module */
+  public static Route = {
+    path: 'assignment-concepts-page',
+    title: 'Assignments Concepts Page Component',
+    component: AssignmentsConceptsPageComponent,
+    canActivate: [courseSitePageGuard(['UTA', 'GTA', 'Instructor'])],
+    resolve: {}
+  };
+}
